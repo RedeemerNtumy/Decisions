90c12d48be05a8e46a9da8e08e53119c79b8dbe4
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-08-12 21:56:06 +0000

    Created the story class with its properties

--- lib/story.dart
+++ lib/story.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:124aba0898c7c1692f5aed90702bed5123418ce7f592e8f2b33ff71470ee3e3d
-size 342
+oid sha256:cf4dcb761c79c8af1f0245eba9cae9d44a57832ffac5729527fd56ece91852f5
+size 130
