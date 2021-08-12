62891a02ebcbefb71f15e20a3e5b48884804f0bc
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-08-12 22:58:17 +0000

    Results of users choices

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c28227c9a14a6e10d7b4687bec19ca33568673240c0cf55c1189d3dc9af00a88
-size 4019
+oid sha256:a37065f2abd77713fc4198f6d4c09aa15ca1be7a1822c0f24eb62e6598822800
+size 4142
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24360e057decc1b99a119f6f65d0885711e46643accdfaa63a8f16b900b25346
-size 4054
+oid sha256:bafc8a18701e46cb476c6e9bc7f99e6a1d2d6b56eb5877de3ccdd75d241b4954
+size 2769
