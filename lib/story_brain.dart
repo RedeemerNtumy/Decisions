dd50380a9334770532c084455a2871b19bfc89b7
RedeemerNtumy <ntumyredeemer@gmail.com>
2021-08-12 22:29:48 +0000

    Updated the app with some more functionality

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cedc298c8014c3a296a2de1e539171d2d14eedba94648424d7cc0b7bdb3e78d4
-size 4008
+oid sha256:24360e057decc1b99a119f6f65d0885711e46643accdfaa63a8f16b900b25346
+size 4054
